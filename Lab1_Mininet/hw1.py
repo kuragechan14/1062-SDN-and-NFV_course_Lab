2023a5b2e0eea06cbc9a589f99d19cacc92f283b
kuragechan14 <kuragechan14@gmail.com>
2018-03-23 21:07:08 +0800

    Update the final version, add port num of controller to fix the bug.

--- Lab1_Mininet/hw1.py
+++ Lab1_Mininet/hw1.py
@@ -16,42 +16,42 @@ class MyTopo( Topo ):
         # Initialize topology
         Topo.__init__( self )
 
-        CoreSwitch=[]
-        AggrSwitch=[]
-        EdgeSwitch=[]
-        Host=[]
+        switch_core=[]
+        switch_aggr=[]
+        switch_edge=[]
+        host=[]
 
         # Add hosts and switches
         for c in range(4):
-            c_tmp=self.addSwitch('s_c%s'%(c+1))   # 4 core switches
-            CoreSwitch.append(c_tmp)
+            c_tmp=self.addSwitch('s'+str(c+1))   # 4 core switches
+            switch_core.append(c_tmp)
         for a in range(8):
-            a_tmp=self.addSwitch('s_a%s'%(a+1))   # 8 aggr switches
-            AggrSwitch.append(a_tmp)
+            a_tmp=self.addSwitch('s'+str(a+5))   # 8 aggr switches
+            switch_aggr.append(a_tmp)
             if (a%2)==0:
-                self.addLink(CoreSwitch[0],a_tmp,bw=1000,loss=2)
-                self.addLink(CoreSwitch[1],a_tmp,bw=1000,loss=2)   #Link aggr to core
+                self.addLink(switch_core[0],a_tmp,bw=1000,loss=2)
+                self.addLink(switch_core[1],a_tmp,bw=1000,loss=2)   #Link aggr to core
             else:
-                self.addLink(CoreSwitch[2],a_tmp,bw=1000,loss=2)
-                self.addLink(CoreSwitch[3],a_tmp,bw=1000,loss=2)
+                self.addLink(switch_core[2],a_tmp,bw=1000,loss=2)
+                self.addLink(switch_core[3],a_tmp,bw=1000,loss=2)
         for e in range(8):
-            e_tmp=self.addSwitch('s_e%s'%(e+1))   # 8 edge switches
-            EdgeSwitch.append(e_tmp)
-            self.addLink(AggrSwitch[e],e_tmp,bw=100)
+            e_tmp=self.addSwitch('s'+str(e+13))   # 8 edge switches
+            switch_edge.append(e_tmp)
+            self.addLink(switch_aggr[e],e_tmp,bw=100)
             if (e%2)==0:
-                self.addLink(AggrSwitch[e+1],e_tmp,bw=100)     #Link edge to aggr
+                self.addLink(switch_aggr[e+1],e_tmp,bw=100)     #Link edge to aggr
             else:
-                self.addLink(AggrSwitch[e-1],e_tmp,bw=100)
+                self.addLink(switch_aggr[e-1],e_tmp,bw=100)
             for h in range(2):
-                h_tmp=self.addHost('h%s'%((2*e)+h+1))
-                Host.append(h_tmp)
+                h_tmp=self.addHost('h'+str((2*e)+h+1))
+                host.append(h_tmp)
                 self.addLink(e_tmp,h_tmp,bw=100)   #Link host to edge
 
 def perfTest():
     "Create network and rum simple performance test"
     topo=MyTopo()
     net=Mininet(topo=topo,link=TCLink,controller=None)
-    net.addController('floodlight',controller=RemoteController,ip='127.0.0.1')
+    net.addController('c0',controller=RemoteController,ip='127.0.0.1',port=6653)
     net.start() #Start Mininet
     print('Dumping host connections')
     dumpNodeConnections(net.hosts)
@@ -59,12 +59,12 @@ def perfTest():
     net.pingFull()   #Do pingFull
     # print('Test bandwidth between h1 and h2')
     h1,h2,h15=net.get('h1','h2','h15')    #Get nodes by name
-    h1.cmdPrint('iperf -s -u -I 1')     #pod 0: h1 server
-    h15.cmdPrint('iperf -s -u -I 1')    #pod 3: h15 server
-    h1.popen('iperf -s -u -I 1'+' > 0656526_pod0_server_report',shell=True)
-    h15.popen('iperf -s -u -I 1'+' > 0656526_pod3_server_report',shell=True)
-    h2.cmdPrint('iperf -c '+h1.IP()+' -u -t 10 -I 1 -b 100m'+ ' > 0656526_pod0_connect_report')   #pod 0: h2 client
-    h2.cmdPrint('iperf -c '+h15.IP()+' -u -t 10 -I 1 -b 100m'+ ' > 0656526_pod3_connect_report')
+    h1.cmdPrint('iperf -s -u -D -i 1')     #pod 0: h1 server
+    #h15.cmdPrint('iperf -s -u -i 1')    #pod 3: h15 server
+    #h1.popen('iperf -s -u -i 1'+' > 0656526_pod0_server_report',shell=True)
+    h15.popen('iperf -s -u -i 1'+' > 0656526_pod3_server_report',shell=True)
+    h2.cmdPrint('iperf -c '+h1.IP()+' -u -t 10 -i 1 -b 100m'+ ' > 0656526_pod0_connect_report')   #pod 0: h2 client
+    h2.cmdPrint('iperf -c '+h15.IP()+' -u -t 10 -i 1 -b 100m'+ ' > 0656526_pod3_connect_report')
 
     net.stop()  #stop Mininet
 
